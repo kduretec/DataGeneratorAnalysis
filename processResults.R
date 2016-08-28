2d24c52d4b6782d8145b870426d483b9fadd135b
Krešimir Đuretec <kresimir.duretec@gmail.com>
2016-08-28 19:29:05 +0200

    extracting feature vectors for test cases

--- processResults.R
+++ processResults.R
@@ -12,17 +12,23 @@ results <-
 sD <- results[results$Element=="size",]
 sD$Value <- as.numeric(sD$Value)
 sD$Value <- sD$Value / 1024 
-sizeHist <- ggplot(sD, aes(x=Value)) + geom_histogram(binwidth = 200)
+sizeHist <- ggplot(sD, aes(x=Value)) + geom_histogram()
 png(filename = paste(pathPlots, "sizeDist.png", sep=""), width = 1800, height = 1200, res=300)
 print(sizeHist)
 dev.off()
 
 uniqDocName <- unique(results$Name)
-swSC <- data.frame(Name=uniqDocName, Size=rep(0,length(uniqDocName)), Word=rep(0,length(uniqDocName)), Format=rep(0,length(uniqDocName)))
-for (nam in unique(results$Name)) {
-  swSC[swSC$Name==nam,]$Size <- results[results$Element=="size" & results$Name==nam,]$Value
-  swSC[swSC$Name==nam,]$Word <- results[results$Element=="GT-wordcount" & results$Name==nam,]$Value
-  swSC[swSC$Name==nam,]$Format <- results[results$Element=="GT-format" & results$Name==nam,]$Value
+featuresElements <- c("GT-pagecount", "GT-paragraphcount", "GT-wordcount")
+swSC <- data.frame(TestCase=uniqDocName)
+for (feat in featuresElements) {
+  swSC[[feat]] <- rep(NA,length(uniqDocName))
+}
+
+for (nam in uniqDocName) {
+  for (feat in featuresElements) {
+    swSC[swSC$TestCase==nam,][[feat]] <- results[results$Name==nam & results$Element==feat,]$Value
+  }
+  
 }
 
 swSC <- swSC[swSC$Format=="doc",]
