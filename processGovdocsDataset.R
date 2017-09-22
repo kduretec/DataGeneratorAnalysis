2246b8102ef24dd798495e5ef9f78a6cc3fa922c
Krešimir Đuretec <kresimir.duretec@gmail.com>
2017-09-22 17:00:44 +0200

    calculating distribution

--- processGovdocsDataset.R
+++ processGovdocsDataset.R
@@ -29,7 +29,7 @@ fileMetadata$numTables <- as.numeric(fileMetadata$numTables)
 fileMetadata$numWords <- as.numeric(fileMetadata$numWords)
 
 paragPageScatter <- ggplot(fileMetadata, aes(x=numPage, y=numParag)) + geom_point() +
-  scale_x_continuous(limits = c(0,1000)) + 
+  scale_x_continuous(limits = c(0,5000)) + 
   scale_y_continuous(limits = c(0,5000))
 paragPageScatter
 
@@ -44,3 +44,28 @@ paragHist
 
 pageHist <- ggplot(fileMetadata, aes(x=numPage)) + geom_histogram()
 pageHist
+
+
+assignClass <- function(bins, value, nameV) {
+  return (bins[value[[nameV]] >= bins$start & value[[nameV]] <= bins$end,]$code)
+}
+
+valueFrequencies <- function(fileBins, met, nameV) {
+  bins <- read.table(fileBins, header=TRUE, sep="\t", stringsAsFactors = FALSE)
+  bins$start <- as.numeric(bins$start)
+  bins$end <- as.numeric(bins$end)
+  bins$code <- as.numeric(bins$code)
+  met$code <- apply(met, 1, function(x) assignClass(bins, x, nameV))
+  met$code <- as.character(met$code)
+  newFrame <- aggregate(met$code, list(code=met$code), FUN=length)
+  
+  newFrame <- merge(bins, newFrame, by="code")
+  
+  barPlot <- ggplot(newFrame, aes(x=title, y=x)) + geom_bar(stat="identity")
+  barPlot
+}
+
+
+
+
+
