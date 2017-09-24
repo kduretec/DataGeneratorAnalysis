d5e39836766cd40541d54ea959c22efe78c85a07
Krešimir Đuretec <kresimir.duretec@gmail.com>
2017-09-24 11:45:15 +0200

    small updates to processing metadata files - logging info

--- processGovdocsDataset.R
+++ processGovdocsDataset.R
@@ -10,24 +10,28 @@ listFiles <- list.files(folderIN)
 fileMetadata <- data.frame(fileName=character(), numPage=character(), numCh=character(), numWords=character(), 
                            numLines=character(), numParag=character(), numTables=character())
 
+numFiles <- length(listFiles)
 count <- 0
 for (f in listFiles) {
+  if (count %% 1000 == 0) 
+    print (paste("Processed ", count, "/", numFiles, sep=""))
   count <- count + 1
   filNameList <- unlist(strsplit(f, ".", fixed=TRUE))
   name <- filNameList[1]
   extension <- filNameList[2]
   fileResult <- paste(folderIN, f, sep="")
   if (file.exists(fileResult)) {
-    print(fileResult)
+    #print(fileResult)
     tmp <- readVBSMetadata(fileResult, name)
     if (is.null(tmp)) next
     fileMetadata <- rbind(fileMetadata, tmp) 
   }
-  if (count == 20 ) {
+  if (count == 2000) {
     break 
   }
 }
 
+print(paste("Processed ", count, " files", sep=""))
 
 fileOut <- "/Users/kresimir/Dropbox/Work/Projects/BenchmarkDP/publications/INFSOF/experiments/real world dataset/metadata.tsv"
 write.table(fileMetadata, fileOut, sep="\t", col.names = TRUE, row.names = FALSE)
