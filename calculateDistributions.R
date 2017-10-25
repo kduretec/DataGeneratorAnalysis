ce1ff6cce37a96f726011698d63689be616d3604
Krešimir Đuretec <kresimir.duretec@gmail.com>
2017-10-25 23:51:12 +0200

    adding representative sampling

--- calculateDistributions.R
+++ calculateDistributions.R
@@ -7,6 +7,8 @@ tsvPath <- "/Users/kresimir/Dropbox/Work/Projects/BenchmarkDP/publications/INFSO
 
 distFolder <- "/Users/kresimir/Dropbox/Work/Projects/BenchmarkDP/publications/INFSOF/experiments/real world dataset/distributions/"
 
+samplesPath <- paste(distFolder, "samples.tsv", sep="")
+  
 fileMetadata <- read.table(tsvPath, header=TRUE, sep="\t", stringsAsFactors = FALSE)
 
 fileMetadata$numPage <- as.numeric(fileMetadata$numPage)
@@ -34,20 +36,54 @@ dev.off()
 
 #page distributions
 binPath <- "input/pageBins.txt"
-binFrame <- valueFrequencies(binPath, fileMetadata, "numPage")
-saveBins(distFolder, "pageDist", binFrame)
+binFrameNPag <- valueFrequencies(binPath, fileMetadata, "numPage")
+saveBins(distFolder, "pageDist", binFrameNPag)
+binFrameNPag$feature <- "numPage"
 
 #word distributions
 binPath <- "input/wordBins.txt"
-binFrame <- valueFrequencies(binPath, fileMetadata, "numWords")
-saveBins(distFolder, "wordDist", binFrame)
+binFrameNWo <- valueFrequencies(binPath, fileMetadata, "numWords")
+saveBins(distFolder, "wordDist", binFrameNWo)
+binFrameNWo$feature <- "numWords"
 
 #paragraph distributions
 binPath <- "input/paragraphBins.txt"
-binFrame <- valueFrequencies(binPath, fileMetadata, "numParag")
-saveBins(distFolder, "paragDist", binFrame)
+binFrameNPar <- valueFrequencies(binPath, fileMetadata, "numParag")
+saveBins(distFolder, "paragDist", binFrameNPar)
+binFrameNPar$feature <- "numParag"
 
 #table distributions
 binPath <- "input/tableBins.txt"
-binFrame <- valueFrequencies(binPath, fileMetadata, "numTables")
-saveBins(distFolder, "tableDist", binFrame)
+binFrameNTab <- valueFrequencies(binPath, fileMetadata, "numTables")
+saveBins(distFolder, "tableDist", binFrameNTab)
+binFrameNTab$feature <- "numTables"
+
+allBinsFrame <- rbind(binFrameNPag, binFrameNPar, binFrameNTab, binFrameNWo) 
+# calculate representative samples 
+combinationFrame <- expand.grid(numParag=binFrameNPar$title, numPage=binFrameNPag$title, 
+                                numWords=binFrameNWo$title, numTables=binFrameNTab$title)
+
+numProp <- c("numPage", "numWords", "numParag", "numTables")
+samples <- apply(combinationFrame, 1, function(x) calculateSamples(x, allBinsFrame, fileMetadata, numProp, samplesPath))
+#sampleFrame <- rbind(samples)
+samples <- samples[!is.na(samples)]
+calculateSamples <- function(row, bins, metadata , names, path) {
+  #print(row)
+  met <- metadata
+  for (prop in names) {
+    #print(prop)
+    start <- bins[bins$feature==prop & bins$title==row[[prop]], ]$start 
+    end <- bins[bins$feature==prop & bins$title==row[[prop]], ]$end
+    met <- met[met[[prop]] >= start & met[[prop]] <= end,]
+  }
+  num <- nrow(met)
+  #print(num)
+  if (num==0) {return (NA)}
+  s <- met[sample(1:num,1), ]
+  #print(s[1,]$fileName)
+  #s$numSamples <- num
+  return (s[1,]$fileName)
+}
+
+
+
